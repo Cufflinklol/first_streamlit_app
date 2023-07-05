70b48587bb5d701976ed73735ac58cd360254395
Cufflinklol <137727624+Cufflinklol@users.noreply.github.com>
2023-07-05 12:43:24 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-
+except URLError as e:
+    streamlit.error()
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -31,8 +32,7 @@ try:
         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
         streamlit.dataframe(fruityvice_normalized)
 
-except URLError as e:
-    streamlit.error()
+
 streamlit.write ('The user entered', fruit_choice)
 
 
