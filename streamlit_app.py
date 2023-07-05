f0e8ec94fb78d44a8059f18cd23b8dd1135ca992
Cufflinklol <137727624+Cufflinklol@users.noreply.github.com>
2023-07-04 22:07:39 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,5 +24,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-/home/appuser/venv/bin/python -m pip install --upgrade pip
 import snowflake.connector
